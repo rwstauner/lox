9edae5abe70de02c9f4158ab9594ccaf5369e225
Randy Stauner <randy@r4s6.net>
2025-05-06 11:58:10 -0700

    Add initial expression types

--- ruby/lib/lox.rb
+++ ruby/lib/lox.rb
@@ -4,5 +4,6 @@ module Lox
   autoload :CLI,        "lox/cli"
   autoload :Errors,     "lox/errors"
   autoload :Scanner,    "lox/scanner"
+  autoload :Expr,       "lox/expr"
   autoload :Token,      "lox/token"
 end
---
+++ ruby/lib/lox/expr.rb
@@ -0,0 +1,24 @@
+module Lox
+  class Expr
+    def self.type(name, attrs)
+      class_eval <<~CODE
+        class #{name} < Expr
+          attr_reader #{attrs.map(&:inspect).join(", ")}
+
+          def initialize(#{attrs.join(", ")})
+            #{attrs.map { |att| "@#{att} = #{att}" }.join("\n") }
+          end
+
+          def accept(visitor)
+            visitor.visit_#{name.downcase}(self)
+          end
+        end
+      CODE
+    end
+
+    type :Binary, %i[left operator right]
+    type :Grouping, %i[expression]
+    type :Literal, %i[value]
+    type :Unary, %i[operator right]
+  end
+end
